79d32a2cfced197d1e55ee991b042febeacf7dc8
Mohamed25885 <memo_868@yahoo.com>
2024-04-27 03:48:22 +0300

    remove unneeded lines

--- index.js
+++ index.js
@@ -2,8 +2,6 @@
 import dotenv from "dotenv";
 import { app } from "./app.js";
 import connectDB from "./src/config/db.js";
-import CommentsController from "./src/controllers/comments_controller.js";
-import PostsController from "./src/controllers/posts_controller.js";
 
 dotenv.config();
 
--- src/routes/api/posts_routes.js
+++ src/routes/api/posts_routes.js
@@ -4,20 +4,13 @@ const PostsRoutes = Router();
 
 import PostsController from "./../../controllers/posts_controller.js";
 
-
-
 PostsRoutes.route("/:userId")
   .post(PostsController.addPost)
   .get(PostsController.getAllPosts);
-  
 
 PostsRoutes.route("/:userId/:postId")
-   .get(PostsController.getPost)
-   .delete(PostsController.deletePost)
-   .patch(PostsController.updatePost);
-   
-
-
-
+  .get(PostsController.getPost)
+  .delete(PostsController.deletePost)
+  .patch(PostsController.updatePost);
 
 export { PostsRoutes };
